1bc1d5f603594ea4f72f7811c812f435099fdf66
Tushar Girish <backend.dev@cornerstonex.ai>
2025-01-15 12:36:43 +0530

    Updated code

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const fs = require('fs')
+const axios = require('axios')
 
 const app = express()
 const port = 3000
@@ -15,6 +16,9 @@ app.post('/getAudio', (req, res) => {
     const { audio_base64 } = req.body
     const buffer = Buffer.from(audio_base64, 'base64');
     fs.writeFileSync("audio.wav", buffer);
+    axios.post('http://localhost:8011/A2F/Player/Play', {
+        a2f_player: '/World/audio2face/Player'
+    })
 });
 
 app.listen(port, () => {
