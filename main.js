04fea1b1dad42a6f1d01c75b017e7ffc897ea472
Tushar Girish <backend.dev@cornerstonex.ai>
2025-01-20 13:48:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,12 @@ app.post('/getAudio', async (req, res) => {
     const { audio_base64 } = req.body
     const buffer = Buffer.from(audio_base64, 'base64');
     fs.writeFileSync("audio.wav", buffer);
-    const audioToFaceCall = axios.post('http://localhost:8011/A2F/Player/Play', {
+    await axios.post('http://127.0.0.1:8011/A2F/Player/SetTrack', {
+        a2f_player: '/World/audio2face/Player',
+        file_name: 'C:/Users/Administrator/Downloads/audio-express-main/audio.wav',
+        time_range: [0, -1]
+    })
+    const audioToFaceCall = axios.post('http://127.0.0.1:8011/A2F/Player/Play', {
         a2f_player: '/World/audio2face/Player'
     });
     const unrealCall = axios.put('http://127.0.0.1:30010/remote/preset/Hope_response/function/Hope_response')
